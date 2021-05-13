f9ef7ee2ae493a3e61af734bc48b3c03885707fb
Eppa Lea Turniawan <eppalea@gmail.com>
2021-05-12 21:29:54 -0700

    running sum of 1d array, so solution

---
+++ q1480.js
@@ -0,0 +1,15 @@
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+// Return the running sum of nums.
+
+const runningSum = function(nums) {
+  let result = [];
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+      sum = sum + nums[i];
+      result.push(sum);
+  }
+  return result; 
+};
+
+nums = [1,2,3,4];
+console.log(runningSum);
